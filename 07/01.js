7179a68c3b4f41b79894f53dd4533bd3da2b6501
Thomas Pummer <dev@nullpointer.at>
2021-12-07 09:02:48 +0100

    07 - solution

---
+++ 07/01.js
@@ -0,0 +1,50 @@
+const { spawn } = require('child_process');
+const fs = require('fs');
+const { format } = require('path');
+const { nextTick } = require('process');
+const readline = require('readline');
+
+async function processLineByLine() {
+  const fileStream = fs.createReadStream('07\\input.txt');
+  //const fileStream = fs.createReadStream('07\\testinput.txt');
+
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  let crabs = [];
+  
+  for await (const line of rl) {
+    crabs = line.split(',').map( Number )
+  }
+
+  console.log(crabs.length)
+
+  let min = Math.min(... crabs)
+  let max = Math.max(... crabs)
+  console.log("Min " + min + " Max " + max)
+
+  let resultFuel;
+  let resultSpot;
+  let first = true;
+
+  for(let i = min; i <= max; i++){
+    let fuelUsed = 0;
+    crabs.forEach(crab =>
+      fuelUsed += Math.abs(i - crab)
+    )
+
+    if(first || fuelUsed < resultFuel){
+      resultFuel = fuelUsed
+      resultSpot = i
+      first = false
+    }
+
+    //console.log("pos " + i + " uses fuel " + fuelUsed)
+  }
+  
+  console.log(`After ${resultFuel} fuel used all crabs are at positoin ${resultSpot}.`)
+}
+
+processLineByLine();
